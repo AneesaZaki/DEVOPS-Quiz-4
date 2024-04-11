ecbfa46634dad5c65d78fbc63c627c10cfc8b6b5
Aneesa Zaki <123785725+AneesaZaki@users.noreply.github.com>
2024-04-10 22:57:26 -0400

    Add files via upload
    added ASP Net Web APP

---
+++ DevOpssQuiz4/DevOpssQuiz4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevOpssQuiz4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
